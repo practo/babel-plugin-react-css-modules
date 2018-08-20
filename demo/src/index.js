d25277976467c4ab414eb9ee5e50dd9f808ca99a
Supriya S <supriyas@practo.com>
2018-08-20 16:37:46 +0530

    copy changes from original branch

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,9 +3,11 @@ import ReactDom from 'react-dom';
 import AnonymouseStyleResolution from './components/AnonymouseStyleResolution';
 import NamedStyleResolution from './components/NamedStyleResolution';
 import RuntimeStyleResolution from './components/RuntimeStyleResolution';
+import ClassnameCallExpressionResolution from './components/ClassnameCallExpressionResolution';
 
 ReactDom.render(<div>
   <AnonymouseStyleResolution />
   <NamedStyleResolution />
+  <ClassnameCallExpressionResolution />
   <RuntimeStyleResolution />
 </div>, document.getElementById('main'));
